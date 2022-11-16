b8e2d426618560e8dfe4514a93b6eed9dbd03428
HarisVeli <haroldas.velicka@gmail.com>
2022-11-16 07:14:49 -0500

    Task 9 JS Done

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,18 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor(title, director, budget) {
+      this.title = title;
+      this.director = director;
+      this.budget = budget;
+    }
+    wasExpensive() {
+      return this.budget > 100000000 ? true : false;
+    }
+  }
+  
+  const mo = new Movie("Fast and furious", "Vin Diesel", 1500000000);
+  
+  console.log("mo.wasExpensive() ===", mo.wasExpensive());
