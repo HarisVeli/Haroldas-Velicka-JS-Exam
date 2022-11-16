a2f056850b4031dda9a6b95a0bb3e63d429f4b1b
HarisVeli <haroldas.velicka@gmail.com>
2022-11-16 06:23:30 -0500

    task 6 js done

--- Task 6/script.js
+++ Task 6/script.js
@@ -19,3 +19,8 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
 ];
+
+const getUserAverageAge = (arr) =>
+  arr.reduce((acc, curr, i, arr) => acc + curr.age / arr.length, 0);
+
+const getUsersNames = (arr) => arr.map((user) => user.name);
